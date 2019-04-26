1a45a66ae67c6873194709cd8aa79c97f375bb98
ESPY My Dung <ngocdiepespy@sfr.fr>
2019-04-26 17:55:09 +0200

    tp angular : composant sans event

---
+++ src/app/collegue/collegue.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Collegue } from '../models/Collegue';
+
+@Component({
+  selector: 'app-collegue',
+  templateUrl: './collegue.component.html',
+  styleUrls: ['./collegue.component.css']
+
+
+
+})
+
+export class CollegueComponent implements OnInit {
+  @Input() col:Collegue
+
+
+  //col:Collegue = new Collegue("azigueguagua", "Lovegood", "Luna", "llovegood@rookery.com",new Date(1981,1,15), "à completer")
+  //constructor() { }
+
+  ngOnInit() {}
+
+  
+
+  modifierCollegue(saisie: HTMLInputElement) {
+    //this.Modifier = !this.Modifier;
+
+    saisie.value = '';
+    saisie.focus();
+  }
+
+}
